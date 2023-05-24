de3eac66f90d11e6230e8cdbcd8469b629047e2c
Boris Fritscher <boris@fritscher.ch>
2023-05-24 23:14:17 +0200

    add cors

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import subprocess
 from threading import Thread
 
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
