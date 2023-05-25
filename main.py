6548226d4219738809a60811c230c8c8e8d2c557
Boris Fritscher <boris@fritscher.ch>
2023-05-25 13:41:00 +0000

    webhook

--- main.py
+++ main.py
@@ -1,31 +1,64 @@
 import os
+import requests as request_client
+import json
 import subprocess
 from threading import Thread
 
 from flask import Flask, request, render_template
 from flask_cors import CORS
 
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+
+OVERRIDES = {
+    'project-anemone': '/',
+    'project-bluedaboudidaboudadaboudidabouda': '/',
+    'project-crocodiles': '/',
+    'project-dtf': '/www/',
+    'project-e_tsuki': '/',
+    'project-fennec': '/',
+    'project-g-unit': '/template_django_vue/',
+    'project-helloworld': '/',
+    'project-ilicoptere': '/django-vue/',
+    'project-jungle': '/',
+    'project-komanodragons': '/django-vue/',
+    'project-lima': '/',
+    'project-maverick': '/',
+    'project-nova': '/template-django-vue-main/',
+    'project-omega': '/Omega/',
+    'project-pepperoni': '/Project-Pepperoni/',
+    'project-qarbon': '/'
+}
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    Thread(target=build, kwargs=request.args.to_dict()).start()
+    if request.json.repository.name.startswith("project") and request.json.ref == "refs/heads/main":
+        commit_id = None
+        if request.json.head_commit:
+            commit_id = request.json.head_commit.id
+        repo = request.json.repository.name
+        org = request.json.repository.organization
+        override = OVERRIDES.get(repo, "")
+        Thread(target=build, kwargs={
+               "org": org, "repo": repo, "override": override, "commit_id": commit_id}).start()
 
-    return f'OK {request.args.to_dict()}'
+    return 'OK'
+
 
 @app.route('/build', methods=['GET'])
 def build_sync():
     build(**request.args.to_dict())
     return f'OK {request.args.to_dict()}'
 
-def build(org, repo, override=''):
-    GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+
+def build(org, repo, override='', commit_id=None):
     repo_lower = repo.lower().replace('project-', '').replace('_', '-')
 
     image_name = repo
@@ -75,12 +108,13 @@ def build(org, repo, override=''):
             "-l", f"traefik.http.routers.{repo_lower}_secured.tls.certresolver=myresolver",
             "--network", "web"
         ]
-
+    state = 'failure'
     if result.returncode == 0:
         app.logger.info(f'BUILD {repo} SUCCESS')
         result = subprocess.run(['docker', 'run', '-d', '--rm', '--name', repo, '-e',
                                  f'DJANGO_ALLOWED_HOST=https://{repo_lower}.rxq.ch']
-                                 + labels(repo_lower, 8000) + [image_name])
+                                + labels(repo_lower, 8000) + [image_name])
+        state = 'success'
     else:
         app.logger.info(f'BUILD {repo} FAILED')
         # serve logs with Nginx
@@ -92,6 +126,26 @@ def build(org, repo, override=''):
                        repo]
                        + labels(repo_lower, 80) + [image_name])
     app.logger.info(f'CONTAINER {repo} STARTED')
+    if commit_id:
+        update_status(org, repo, repo_lower, commit_id, state)
+
+
+def update_status(org, repo, repo_lower, commit_id, state):
+
+    url = f'https://api.github.com/repos/{org}/{repo}/statuses/{commit_id}'
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'state': state,
+        'target_url': f'https://{repo_lower}.rxq.ch/',
+        'description': 'Builder status',
+        'context': 'continuous-integration/interschool'
+    }
+    request_client.post(url, headers=headers,
+                        data=json.dumps(data), timeout=10)
 
 
 if __name__ == '__main__':
