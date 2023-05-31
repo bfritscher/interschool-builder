718e8405da1946af5dbf3c9ae3ef7d621112f96d
Boris Fritscher <boris@fritscher.ch>
2023-05-31 19:28:52 +0000

    Build Debian of alpine fail

---
+++ build_templates/Build.Debian.Dockerfile
@@ -0,0 +1,26 @@
+FROM node:18 AS frontend
+WORKDIR /app
+COPY package.json /app/package.json
+RUN npm install
+COPY . /app
+RUN sed -i 's/baseURL: .*/baseURL: "\/api",/' src/services/api.js
+RUN npm run build
+
+FROM python:3.11
+ENV DJANGO_SUPERUSER_USERNAME=autoadmin
+ENV DJANGO_SUPERUSER_EMAIL=autoadmin@example.com
+ENV DJANGO_SUPERUSER_PASSWORD=heg
+ENV DJANGO_SUPERUSER_FIRST_NAME=auto
+ENV DJANGO_SUPERUSER_LAST_NAME=admin
+WORKDIR /app
+COPY requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
+COPY . /app
+RUN sed -i '/django.middleware.clickjacking.XFrameOptionsMiddleware/d' backend/settings/base.py
+COPY --from=frontend /app/dist /app/dist
+
+RUN python manage.py migrate
+RUN python manage.py createsuperuser --no-input
+RUN python manage.py loaddata ./backend/*/*fixture*/*.json; exit 0
+RUN cat /app/prod.py >> backend/settings/base.py
+CMD python manage.py runserver 0.0.0.0:8000
--- main.py
+++ main.py
@@ -59,7 +59,6 @@ def build_sync():
     build(**request.args.to_dict())
     return f'OK {request.args.to_dict()}'
 
-
 def build(org, repo, override='', commit_id=None):
     repo_lower = repo.lower().replace('project-', '').replace('_', '-')
 
@@ -79,54 +78,59 @@ def build(org, repo, override='', commit_id=None):
         -H \'Cache-Control: no-cache\' https://api.github.com/repos/{org}/{repo}/tarball | tar xz --strip-components=1'])
 
     # Copy the Dockerfile into the repository
-    subprocess.run(['cp', '/app/build_templates/Build.Dockerfile',
-                   f'./Dockerfile'], cwd=workdir)
     subprocess.run(['cp', '/app/build_templates/dockerignore',
                    f'./.dockerignore'], cwd=workdir)
     subprocess.run(['cp', '/app/build_templates/prod.py',
                    f'./prod.py'], cwd=workdir)
 
-    # Build the Docker image and save the logs to a file
-    app.logger.info(f'BUILD {repo} STARTED')
-    result = subprocess.run(
-        ['docker', 'build', '-t', image_name, '.'], capture_output=True, cwd=workdir)
-    with open(f'./data/{repo}{override}/build.log', 'wb') as f:
-        f.write(result.stdout)
-        f.write(result.stderr)
+    def run_build_cmd(image_type):
+        # Build the Docker image and save the logs to a file
+        subprocess.run(['cp', f'/app/build_templates/Build.{image_type}.Dockerfile',
+                    f'./Dockerfile'], cwd=workdir)
+        app.logger.info(f'BUILD {repo} STARTED')
+        result = subprocess.run(
+            ['docker', 'build', '-t', image_name, '.'], capture_output=True, cwd=workdir)
+        with open(f'./data/{repo}{override}/build.log', 'wb') as f:
+            f.write(result.stdout)
+            f.write(result.stderr)
 
-    def labels(repo_lower, port):
-        return [
-            "-l", "traefik.enable=true",
-            "-l", "traefik.docker.network=web",
-            "-l", "traefik.http.middlewares.https_redirect.redirectscheme.scheme=https",
-            "-l", "traefik.http.middlewares.https_redirect.redirectscheme.permanent=true",
-            "-l", f"traefik.http.services.{repo_lower}.loadbalancer.server.port={port}",
-            "-l", f"traefik.http.routers.{repo_lower}.rule=Host(`{repo_lower}.rxq.ch`)",
-            "-l", f"traefik.http.routers.{repo_lower}.entrypoints=web",
-            "-l", f"traefik.http.routers.{repo_lower}.middlewares=https_redirect",
-            "-l", f"traefik.http.routers.{repo_lower}_secured.rule=Host(`{repo_lower}.rxq.ch`)",
-            "-l", f"traefik.http.routers.{repo_lower}_secured.entrypoints=websecure",
-            "-l", f"traefik.http.routers.{repo_lower}_secured.tls=true",
-            "-l", f"traefik.http.routers.{repo_lower}_secured.tls.certresolver=myresolver",
-            "--network", "web"
-        ]
-    state = 'failure'
-    if result.returncode == 0:
-        app.logger.info(f'BUILD {repo} SUCCESS')
-        result = subprocess.run(['docker', 'run', '-d', '--rm', '--name', repo, '-e',
-                                 f'DJANGO_ALLOWED_HOST=https://{repo_lower}.rxq.ch']
-                                + labels(repo_lower, 8000) + [image_name])
-        state = 'success'
-    else:
-        app.logger.info(f'BUILD {repo} FAILED')
-        # serve logs with Nginx
-        subprocess.run(
-            ['sh', '-c', 'cp /app/build_templates/Nginx.Dockerfile ./Dockerfile && cp /app/build_templates/index.html ./index.html'], cwd=workdir)
-        subprocess.run(['docker', 'build', '-t', image_name,
-                       '.'], capture_output=True, cwd=workdir)
-        subprocess.run(['docker', 'run', '-d', '--rm', '--name',
-                       repo]
-                       + labels(repo_lower, 80) + [image_name])
+        def labels(repo_lower, port):
+            return [
+                "-l", "traefik.enable=true",
+                "-l", "traefik.docker.network=web",
+                "-l", "traefik.http.middlewares.https_redirect.redirectscheme.scheme=https",
+                "-l", "traefik.http.middlewares.https_redirect.redirectscheme.permanent=true",
+                "-l", f"traefik.http.services.{repo_lower}.loadbalancer.server.port={port}",
+                "-l", f"traefik.http.routers.{repo_lower}.rule=Host(`{repo_lower}.rxq.ch`)",
+                "-l", f"traefik.http.routers.{repo_lower}.entrypoints=web",
+                "-l", f"traefik.http.routers.{repo_lower}.middlewares=https_redirect",
+                "-l", f"traefik.http.routers.{repo_lower}_secured.rule=Host(`{repo_lower}.rxq.ch`)",
+                "-l", f"traefik.http.routers.{repo_lower}_secured.entrypoints=websecure",
+                "-l", f"traefik.http.routers.{repo_lower}_secured.tls=true",
+                "-l", f"traefik.http.routers.{repo_lower}_secured.tls.certresolver=myresolver",
+                "--network", "web"
+            ]
+        state = 'failure'
+        if result.returncode == 0:
+            app.logger.info(f'BUILD {repo} SUCCESS')
+            result = subprocess.run(['docker', 'run', '-d', '--rm', '--name', repo, '-e',
+                                    f'DJANGO_ALLOWED_HOST=https://{repo_lower}.rxq.ch']
+                                    + labels(repo_lower, 8000) + [image_name])
+            state = 'success'
+        else:
+            app.logger.info(f'BUILD {repo} FAILED')
+            if image_type == "Alpine":
+                return run_build_cmd("Debian")
+            # serve logs with Nginx
+            subprocess.run(
+                ['sh', '-c', 'cp /app/build_templates/Nginx.Dockerfile ./Dockerfile && cp /app/build_templates/index.html ./index.html'], cwd=workdir)
+            subprocess.run(['docker', 'build', '-t', image_name,
+                        '.'], capture_output=True, cwd=workdir)
+            subprocess.run(['docker', 'run', '-d', '--rm', '--name',
+                        repo]
+                        + labels(repo_lower, 80) + [image_name])
+        return state
+    state = run_build_cmd("Alpine")
     app.logger.info(f'CONTAINER {repo} STARTED')
     if commit_id:
         update_status(org, repo, repo_lower, commit_id, state)
