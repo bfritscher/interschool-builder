6d7c0ef4c11ae969c033e350ef48bf35fb20cde7
Boris Fritscher <boris@fritscher.ch>
2025-07-03 15:09:47 +0200

    video from 2025

--- videos/convert.bat
+++ videos/convert.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "input_dir=C:\temp\demo_videos"
-set "output_dir=C:\temp\out"
+set "input_dir=E:\projects\interschool-builder\videos\in"
+set "output_dir=E:\projects\interschool-builder\videos\2025"
 
 for %%f in ("%input_dir%\*.*") do (
     set "input_file=%%~ff"
     set "output_file=!output_dir!\%%~nf_cap.mp4"
-    ffmpeg -i "!input_file!" -filter:v "setpts=PTS/5" -an "!output_file!"
+    ffmpeg -i "!input_file!" -filter:v "setpts=PTS/5,scale=1280:720" -c:v libx264 -crf 23 -preset slow -profile:v main -an "!output_file!"
 )
 
 echo All files processed.
